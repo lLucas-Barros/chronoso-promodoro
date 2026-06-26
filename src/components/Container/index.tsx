4878f0e47d4af348bb0df4d3d483f5c6f20c6fb5
Lucas Barros <lucasgleyfson59@gmail.com>
2026-06-26 15:24:06 -0300

    ajustando a seção de icons

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,29 @@
+import type React from "react";
+import styles from './styles.module.css'
+
+
+type ContainerProps = {
+  children: React.ReactNode;
+}
+
+
+
+export function Container({ children }: ContainerProps) {
+
+  console.log(children)
+
+  return (
+
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+
+  )
+
+
+
+
+
+}
